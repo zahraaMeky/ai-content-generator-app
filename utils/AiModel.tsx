0d8e1804e8fb87a134a6bcbc36dc56545d0db701
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-07-01 23:27:26 +0400

    Update AiModel.tsx

--- utils/AiModel.tsx
+++ utils/AiModel.tsx
@@ -1,25 +1,42 @@
-require('dotenv').config();
-import { GoogleGenerativeAI } from "@google/generative-ai";
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
 
-const geminiApiKey = process.env.GEMINI_API_KEY;
-
-const genAI = new GoogleGenerativeAI(geminiApiKey);
-
-const model = genAI.getGenerativeModel({
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+  
+  const apiKey = process.env.GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
-});
-
-const generationConfig = {
+  });
+  
+  const generationConfig = {
     temperature: 1,
     topP: 0.95,
     topK: 64,
     maxOutputTokens: 8192,
     responseMimeType: "text/plain",
-};
+  };
+  
 
-export const chatSession = model.startChat({
-    generationConfig,
-    // safetySettings: Adjust safety settings
-    // See https://ai.google.dev/gemini-api/docs/safety-settings
-    history: [],
-});
+    export const chatSession = model.startChat({
+      generationConfig,
+   // safetySettings: Adjust safety settings
+   // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+      ],
+    });
+  
+   
+  
+  
