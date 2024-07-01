90a238fd07b4790c3737653b554605d548faaaa9
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-07-01 22:17:00 +0400

    update AiModel.tsx

--- utils/AiModel.tsx
+++ utils/AiModel.tsx
@@ -1,42 +1,29 @@
-/*
- * Install the Generative AI SDK
- *
- * $ npm install @google/generative-ai
- *
- * See the getting started guide for more information
- * https://ai.google.dev/gemini-api/docs/get-started/node
- */
+require('dotenv').config();
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } = require("@google/generative-ai");
-  
-  const apiKey = process.env.GEMINI_API_KEY;
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
+const geminiApiKey = process.env.GEMINI_API_KEY;
+
+if (!geminiApiKey) {
+    throw new Error('GEMINI_API_KEY is not defined in the environment.');
+}
+
+const genAI = new GoogleGenerativeAI(geminiApiKey);
+
+const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
-  });
-  
-  const generationConfig = {
+});
+
+const generationConfig = {
     temperature: 1,
     topP: 0.95,
     topK: 64,
     maxOutputTokens: 8192,
     responseMimeType: "text/plain",
-  };
-  
+};
 
-    export const chatSession = model.startChat({
-      generationConfig,
-   // safetySettings: Adjust safety settings
-   // See https://ai.google.dev/gemini-api/docs/safety-settings
-      history: [
-      ],
-    });
-  
-   
-  
-  
+export const chatSession = model.startChat({
+    generationConfig,
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [],
+});
